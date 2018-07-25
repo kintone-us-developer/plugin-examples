7909b36be3b8ae163ad33c7aa3b31b4b37b08e16
Tomoki Kyotani <tkyotani121@gmail.com>
2018-07-25 15:35:07 -0700

    English Localization

---
+++ examples/textConnect_English/config_js/config.js
@@ -0,0 +1,169 @@
+/*
+ * textConnect Plug-in
+ * Copyright (c) 2016 Cybozu
+ *
+ * Licensed under the MIT License
+ */
+jQuery.noConflict();
+(function($, PLUGIN_ID) {
+    'use strict';
+
+    // set the pluginID
+    var CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
+
+    function decodeSpace(htmlstr) {
+        return htmlstr.replace(/&nbsp;/g, ' ').replace(/&emsp;/g, '　');
+    }
+
+    function setDefault() {
+        if (CONF) {
+            for (var i = 1; i < 16; i++) {
+                $('#select' + i).val(CONF['select' + i]);
+            }
+
+            // get the previous plugin setting
+            if (CONF.hasOwnProperty('line_number')) {
+                $('#copyfield1').val(CONF.copyfield);
+                if (CONF.copyfield !== '') {
+                    $('#between1').val(decodeSpace(CONF.between));
+                } else {
+                    $('#between1').val(CONF.between);
+                }
+            } else {
+                $('#copyfield1').val(CONF.copyfield1);
+                $('#copyfield2').val(CONF.copyfield2);
+                $('#copyfield3').val(CONF.copyfield3);
+                for (var y = 1; y < 4; y++) {
+                    if (CONF['copyfield' + y] !== '') {
+                        $('#between' + y).val(decodeSpace(CONF['between' + y]));
+                    } else {
+                        $('#between' + y).val(CONF['between' + y]);
+                    }
+                }
+            }
+        }
+    }
+
+    function escapeHtml(htmlstr) {
+        return htmlstr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+            .replace(/'/g, '&quot;').replace(/'/g, '&#39;');
+    }
+
+    function encodeSpace(htmlstr1) {
+        return htmlstr1.replace(/\u0020/g, '&nbsp;').replace(/\u3000/g, '&emsp;');
+    }
+
+
+    function setDropdown() {
+        // get the form fields info and put them in the selection boxes
+        var url = kintone.api.url('/k/v1/preview/app/form/fields', true);
+        kintone.api(url, 'GET', {'app': kintone.app.getId()}, function(resp) {
+            var $option = $('<option>');
+            for (var key in resp.properties) {
+                if (!resp.properties.hasOwnProperty(key)) {continue; }
+                var prop = resp.properties[key];
+
+                switch (prop.type) {
+                    // 文字列1行の時と文字列複数行を結合フィールドと保存フィールドに適用
+                    case 'SINGLE_LINE_TEXT':
+                    case 'MULTI_LINE_TEXT':
+                        for (var m = 1; m < 16; m++) {
+                            $option.attr('value', escapeHtml(prop.code));
+                            $option.text(escapeHtml(prop.label));
+                            $('#select' + m).append($option.clone());
+                        }
+                        $('#copyfield1').append($option.clone());
+                        $('#copyfield2').append($option.clone());
+                        $('#copyfield3').append($option.clone());
+
+                        break;
+                    // リッチエディタの時は保存フィールドのみに適用
+                    case 'RICH_TEXT':
+                        for (var l = 1; l < 16; l++) {
+                            $option.attr('value', escapeHtml(prop.code));
+                            $option.text(escapeHtml(prop.label));
+                        }
+                        $('#copyfield1').append($option.clone());
+                        $('#copyfield2').append($option.clone());
+                        $('#copyfield3').append($option.clone());
+                        break;
+
+                    // このパターンの時は結合フィールドのみに適用
+                    case 'DATETIME':
+                    case 'NUMBER':
+                    case 'RADIO_BUTTON':
+                    case 'CHECK_BOX':
+                    case 'MULTI_SELECT':
+                    case 'DROP_DOWN':
+                    case 'DATE':
+                    case 'TIME':
+                    case 'LINK':
+                    case 'USER_SELECT':
+                    case 'ORGANIZATION_SELECT':
+                    case 'GROUP_SELECT':
+                        for (var n = 1; n < 16; n++) {
+                            $option.attr('value', escapeHtml(prop.code));
+                            $option.text(escapeHtml(prop.label));
+                            $('#select' + n).append($option.clone());
+                        }
+                        break;
+
+                    default :
+                        break;
+                }
+            }
+            setDefault();
+        });
+    }
+
+    function checkValues() {
+        // 必須項目のチェック、
+        for (var b = 1; b < 6; b++) {
+            if ($('#select' + b).val() !== '' && $('#copyfield1').val() === '') {
+  // swal('Error!', '「結合された文字列を表示する項目」は必須です。', 'error');
+                swal('Error!', 'Please select a field to display the combined texts.', 'error');
+                return false;
+            }
+        }
+        for (var c = 6; c < 11; c++) {
+            if ($('#select' + c).val() !== '' && $('#copyfield2').val() === '') {
+  // swal('Error!', '「結合された文字列を表示する項目」は必須です。', 'error');
+                swal('Error!', 'Please select a field to display the combined texts.', 'error');
+                return false;
+            }
+        }
+        for (var d = 11; d < 16; d++) {
+            if ($('#select' + d).val() !== '' && $('#copyfield3').val() === '') {
+  // swal('Error!', '「結合された文字列を表示する項目」は必須です。', 'error');
+                swal('Error!', 'Please select a field to display the combined texts.', 'error');
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 「保存する」ボタン押下時に入力情報を設定する
+    $('#submit').click(function() {
+        var config = [];
+        for (var i = 1; i < 16; i++) {
+            config['select' + i] = $('#select' + i).val();
+        }
+        config['copyfield1'] = $('#copyfield1').val();
+        config['copyfield2'] = $('#copyfield2').val();
+        config['copyfield3'] = $('#copyfield3').val();
+        config['between1'] = encodeSpace($('#between1').val());
+        config['between2'] = encodeSpace($('#between2').val());
+        config['between3'] = encodeSpace($('#between3').val());
+
+        if (checkValues()) {
+            kintone.plugin.app.setConfig(config);
+        }
+    });
+
+    // 「キャンセル」ボタン押下時の処理
+    $('#cancel').click(function() {
+        window.history.back();
+    });
+
+    setDropdown();
+})(jQuery, kintone.$PLUGIN_ID);
---
+++ examples/textConnect_English/desktops/desktop.js
@@ -0,0 +1,187 @@
+/*
+ * textConnect Plug-in
+ * Copyright (c) 2016 Cybozu
+ *
+ * Licensed under the MIT License
+ */
+(function(PLUGIN_ID) {
+    'use strict';
+
+    var CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
+    // 設定値読み込み
+    if (!CONF) {
+        return false;
+    }
+
+    if (!CONF.copyfield1) {
+        return;
+    }
+
+    function escapeHtml(htmlstr) {
+        return htmlstr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+            .replace(/'/g, '&quot;').replace(/'/g, '&#39;');
+    }
+
+    function checkTexValue(tex) {
+        var tex_changes = '';
+        // ユーザー選択、組織選択、グループ選択でnameのみを取得する
+        switch (tex['type']) {
+            case 'USER_SELECT':
+            case 'ORGANIZATION_SELECT':
+            case 'GROUP_SELECT':
+                if (tex.value.length !== 0) {
+                    tex_changes = tex['value'][0]['name'];
+                }
+                break;
+
+            // 日時のうち、日付だけをトリムする
+            case 'DATETIME':
+                if (tex.value !== undefined) {
+                    tex_changes = (tex['value']).substr(0, 10);
+                }
+                break;
+
+            // 複数の値の場合は配列の0のみを反映する
+            case 'CHECK_BOX':
+            case 'MULTI_SELECT':
+                tex_changes = tex['value'][0];
+                break;
+
+            // そのほかのすべてのフィールドタイプ
+            default :
+                tex_changes = tex['value'];
+                break;
+        }
+        return tex_changes;
+    }
+
+    // 空のフィールドを探す
+    function fieldValues(record, selectionArry) {
+        var fieldarray = [];
+        for (var j = 0; j < 5; j++) {
+            if (selectionArry[j] !== '') {
+                var tex = record[String(selectionArry[j])];
+                if (tex.value !== undefined) {
+                    fieldarray.push(checkTexValue(tex));
+                } else {
+                    fieldarray.push('');
+                }
+            }
+
+        }
+        return fieldarray;
+    }
+
+    function createSelectionArry() {
+        // 行毎にselectionの配列を作成
+        var selectionArry = [];
+        selectionArry[0] = [];
+        selectionArry[1] = [];
+        selectionArry[2] = [];
+        for (var i = 1; i < 16; i++) {
+            selectionArry[parseInt((i - 1) / 5, 10)].push(CONF['select' + i]);
+        }
+        return selectionArry;
+    }
+
+
+    function connectField(record) {
+        // 各結合項目の処理
+        for (var i = 1; i < 4; i++) {
+            var cdcopyfield = CONF['copyfield' + i];
+            var cdbetween = CONF['between' + i];
+            var selectionArry = createSelectionArry();
+            var joinText = fieldValues(record, selectionArry[i - 1]);
+            if (cdbetween === '&nbsp;') {
+                cdbetween = '\u0020';
+            } else if (cdbetween === '&emsp;') {
+                cdbetween = '\u3000';
+            }
+            if (joinText.length > 0) {
+                record[String(cdcopyfield)]['value'] = String(joinText.join(cdbetween));
+            }
+        }
+    }
+
+    // 値に変更があった場合のイベントと保存前イベント
+    function createEvents() {
+
+        var changeEvent = ['app.record.edit.submit',
+            'app.record.create.submit',
+            'app.record.index.edit.submit'];
+        var edit_change = 'app.record.edit.change.';
+        var create_change = 'app.record.create.change.';
+        var index_change = 'app.record.index.edit.change.';
+
+        for (var a = 1; a < 16; a++) {
+            var target = CONF['select' + a];
+            changeEvent.push(edit_change + escapeHtml(target));
+            changeEvent.push(create_change + escapeHtml(target));
+            changeEvent.push(index_change + escapeHtml(target));
+        }
+        return changeEvent;
+    }
+
+    // 一覧作成編集画面
+    var events1 = ['app.record.edit.show',
+        'app.record.create.show',
+        'app.record.index.edit.show'
+    ];
+
+    // 結合フィールドを入力不可にする
+    kintone.events.on(events1, function(event) {
+        var record = event['record'];
+        for (var i = 1; i < 4; i++) {
+            if (CONF['copyfield' + i] !== '') {
+                record[String(CONF['copyfield' + i])]['disabled'] = true;
+            }
+        }
+        return event;
+    });
+
+    // changeイベントとsubmitイベント発火時に文字結合処理を行う
+    var valevents = createEvents();
+    kintone.events.on(valevents, function connect_texts(event) {
+        var record = event.record;
+        connectField(record);
+        return event;
+    });
+
+
+    // 保存前イベント
+    var submitEvent = ['app.record.edit.submit',
+        'app.record.create.submit',
+        'app.record.index.edit.submit'
+    ];
+
+
+    // 保存ボタンを押下したときに空フィールドが指定されているかを確認
+    kintone.events.on(submitEvent, function(event) {
+        var record = event.record;
+        var selectionArry = createSelectionArry();
+        var flag = false;
+
+        for (var m = 0; m < 3; m++) {
+            var jointext = fieldValues(record, selectionArry[m]);
+            for (var i = 0; i < jointext.length; i++) {
+                if (!jointext[i]) {
+  // var res = confirm('結合対象のフィールドに空文字が含まれています。登録しますか？');
+                    var res = confirm('At least one of fields that will be combined has no value. Do you still wish to connect them?');
+                    if (res === false) {
+  // event.error = 'キャンセルしました';
+                        event.error = 'Canceled!';
+                        return event;
+                    }
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        return event;
+    });
+
+
+})(kintone.$PLUGIN_ID);
